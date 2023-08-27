0ec129326b91db6f0c02972a10671c2958d2c62b
Scott Stevenson <scott@stevenson.io>
2023-08-27 21:09:15 +0100

    Add solution for 2015 day 25

---
+++ 2015/25/day25.py
@@ -0,0 +1,28 @@
+import re
+from pathlib import Path
+from typing import cast
+
+
+def load_input() -> tuple[int, int]:
+    text = Path(__file__).parent.joinpath("input.txt").read_text()
+    if match := re.search(r"row (\d+), column (\d+).", text):
+        return cast(tuple[int, int], tuple(map(int, match.groups())))
+    raise RuntimeError("Invalid input file")
+
+
+def part_1(target_row: int, target_col: int) -> int:
+    row, col, code = 1, 1, 20151125
+    while code := (code * 252533) % 33554393:
+        if row == 1:
+            row, col = col + 1, 1
+        else:
+            row -= 1
+            col += 1
+        if row == target_row and col == target_col:
+            return code
+    raise RuntimeError("Solution not found")
+
+
+if __name__ == "__main__":
+    coordinates = load_input()
+    print("Part 1:", part_1(*coordinates))
