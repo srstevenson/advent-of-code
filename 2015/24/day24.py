116227f23f55574c4f28bee59114eaa8bf20a668
Scott Stevenson <scott@stevenson.io>
2023-08-27 21:06:55 +0100

    Add solution for 2015 day 24

---
+++ 2015/24/day24.py
@@ -0,0 +1,37 @@
+import itertools
+import math
+from collections.abc import Sequence
+from pathlib import Path
+
+
+def load_input() -> list[int]:
+    return list(
+        map(int, Path(__file__).parent.joinpath("input.txt").read_text().splitlines())
+    )
+
+
+def find_min_qe(packages: Sequence[int], groups: int) -> int:
+    weight = sum(packages) // groups
+    for r in range(1, len(packages)):
+        qes = [
+            math.prod(group)
+            for group in itertools.combinations(packages, r=r)
+            if sum(group) == weight
+        ]
+        if qes:
+            return min(qes)
+    raise RuntimeError("Solution not found")
+
+
+def part_1(packages: Sequence[int]) -> int:
+    return find_min_qe(packages, 3)
+
+
+def part_2(packages: Sequence[int]) -> int:
+    return find_min_qe(packages, 4)
+
+
+if __name__ == "__main__":
+    packages = load_input()
+    print("Part 1:", part_1(packages))
+    print("Part 2:", part_2(packages))
